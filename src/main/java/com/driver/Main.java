6e6cc22e9b67b34509f4aa44bd19bb130f58a9dd
Yusuf <mohammad.yusufkhan12345@gmail.com>
2023-04-16 13:14:19 +0530

    encapsulation assignment added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        // error because name has private access in RWOnly class
+        obj.setName("name");
+        System.out.println(obj.getName());
+
+    }
 }
